96aae2c3672fe920bbe883e1c0d2df94f625c96f
Samuel bond <samuel0bond007@gmail.com>
2025-12-06 19:58:43 +0200

    Add files via upload

---
+++ task.py
@@ -0,0 +1,11 @@
+# Learning the very basic print statement
+print("Hello, I heard your cool music, But I hear you don't have a band name yet "
+      "\nLet me help you out with that.....")
+
+city = input("\n\n\n where where you born")
+print("sounds great....")
+pet = input("what was the name of your favorite pet growing up?")
+extra_name = len(pet)
+extra_pet = len(city)
+print("perfect! I think I've got it!")
+print("what about..." + city + pet + str(extra_name) + str(extra_pet))
